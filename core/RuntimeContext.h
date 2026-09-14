85f3b62e3560a6e43a022bbd1b41bb2e7af4c538
mier3379 <fmiller3369@live.hccc.edu>
2026-09-13 23:38:38 -0400

    Connect RuntimeContext with PluginRegistry

--- core/RuntimeContext.h
+++ core/RuntimeContext.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#include "PluginRegistry.h"
+
 class RuntimeContext
 {
 public:
     static RuntimeContext& instance();
 
     bool initialize();
+
+    PluginRegistry& plugins();
+
+private:
+    RuntimeContext();
+
+    PluginRegistry m_pluginRegistry;
 };
