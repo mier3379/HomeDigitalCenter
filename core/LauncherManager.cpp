e11b004db609a2cbed5739744f81f3085a70ba2c
mier3379 <fmiller3369@live.hccc.edu>
2026-09-14 05:37:09 -0400

    feat: implement launcher manager process control

---
+++ core/LauncherManager.cpp
@@ -0,0 +1,42 @@
+#include "LauncherManager.h"
+
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QProcess>
+
+namespace HDC {
+
+bool LauncherManager::loadConfig(const QString &file)
+{
+    QFile config(file);
+    if (!config.open(QIODevice::ReadOnly))
+        return false;
+
+    const auto doc = QJsonDocument::fromJson(config.readAll());
+    const auto apps = doc.object().value("apps").toArray();
+
+    m_apps.clear();
+    for (const auto &item : apps) {
+        auto obj = item.toObject();
+        AppInfo info;
+        info.id = obj.value("id").toString();
+        info.name = obj.value("name").toString();
+        info.path = obj.value("path").toString();
+        m_apps.append(info);
+    }
+
+    return true;
+}
+
+bool LauncherManager::launch(const QString &id)
+{
+    for (const auto &app : m_apps) {
+        if (app.id == id)
+            return QProcess::startDetached(app.path);
+    }
+
+    return false;
+}
+
+}
