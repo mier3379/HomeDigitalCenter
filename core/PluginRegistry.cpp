f44aec7232c0464c672b07d78a111336bff8a928
mier3379 <fmiller3369@live.hccc.edu>
2026-09-13 23:03:55 -0400

    Implement PluginRegistry runtime initialization

---
+++ core/PluginRegistry.cpp
@@ -0,0 +1,28 @@
+#include "PluginRegistry.h"
+
+PluginRegistry::PluginRegistry(QObject* parent)
+    : QObject(parent)
+{
+}
+
+void PluginRegistry::registerPlugin(PluginInterface* plugin)
+{
+    if (plugin && !m_plugins.contains(plugin))
+        m_plugins.append(plugin);
+}
+
+QList<PluginInterface*> PluginRegistry::plugins() const
+{
+    return m_plugins;
+}
+
+bool PluginRegistry::initializePlugins()
+{
+    for (auto* plugin : m_plugins)
+    {
+        if (!plugin->initialize())
+            return false;
+    }
+
+    return true;
+}
